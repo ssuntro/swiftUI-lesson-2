6bfaf5e243b81e2a5590f4aaa70efb206a0c01e5
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 16:24:40 +0700

    init project

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Siroratt Suntronsuk on 18/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
